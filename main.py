32d70fe0e2604f2321016445c24c1ff877378ae9
zro2exp <zro2exp@proton.me>
2025-02-12 00:45:50 -0600

    add split functionality

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
         
